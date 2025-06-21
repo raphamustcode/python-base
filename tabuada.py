481c25bd63017f0d7c84cdaa158c729db0fcdda5
raphaa <artworkrapha@gmail.com>
2025-06-21 19:49:33 -0300

    refatoração e formatação

--- tabuada.py
+++ tabuada.py
@@ -1,16 +1,17 @@
 #!usr/bin/env python
 """Imprime a tabuada do 1 ao 10.
 
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+1 x 1 = 1
+2 x 1 = 2
+3 x 1 = 3
 ...
--------------
-Tabuada do 2
-2
-4
-6
+##################
+---Tabuada do 2---
+
+    2 x 1 = 2
+    2 x 2 = 4
+
 ...
 -------------
 """
@@ -18,15 +19,16 @@ __version__ = "0.1.0"
 __author__ = "Rapha"
 
 # numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numeros = list(range(1, 11))
-
-print(numeros)
-
 # Iterable (percorríveis)
+numeros = list(range(1,11))
 
-# para
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("-------------")
+# para cada numero em numeros:
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
+
+
