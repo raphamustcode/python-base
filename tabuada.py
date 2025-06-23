ca0fefb171cdfb59ac1dad20d3fe1e4da558ef76
raphaa <artworkrapha@gmail.com>
2025-06-23 17:51:43 -0300

    minor adjustments

--- tabuada.py
+++ tabuada.py
@@ -2,9 +2,10 @@
 """Imprime a tabuada do 1 ao 10.
 
 ---Tabuada do 1---
-1 x 1 = 1
-2 x 1 = 2
-3 x 1 = 3
+
+    1 x 1 = 1
+    2 x 1 = 2
+
 ...
 ##################
 ---Tabuada do 2---
@@ -24,11 +25,12 @@ numeros = list(range(1,11))
 
 # para cada numero em numeros:
 for n1 in numeros:
-    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print("{:-^19}".format(f"Tabuada do {n1}"))
     print()
     for n2 in numeros:
         resultado = n1 * n2
-        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
-    print("#" * 18)
+        print("{:^19}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 19)
+
 
 
